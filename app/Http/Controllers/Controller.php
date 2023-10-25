0200fa4c332cbd43144cdff6f6ac1d694db22243
christopher4RY <arkarlin486@gmail.com>
2023-10-25 10:41:21 +0630

    Json Response Example

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,9 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    public function success(mixed $data, string $message = 'Successful', int $status = 200) :JsonResponse
+    {
+        return response()->json([
+            'message'=>$message,
+            'data'=>$data
+        ], $status);
+    }
 }
